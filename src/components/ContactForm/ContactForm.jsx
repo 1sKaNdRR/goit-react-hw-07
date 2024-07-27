d8b8e060874fde7564f7a24748f432c9eff4324c
1sKaNdRR <151454522+1sKaNdRR@users.noreply.github.com>
2024-07-27 12:36:11 +0200

    react-hw-7-7

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,52 +1,35 @@
 import { useDispatch } from 'react-redux';
-import { useId } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import { addContact } from '../../redux/contactsSlice';
+import { addContact } from '../../redux/contactsOps';
 import css from './ContactForm.module.css';
 
 export default function ContactForm() {
   const dispatch = useDispatch();
 
   const UserSchema = Yup.object().shape({
-    name: Yup.string()
-      .min(3, "Too Short")
-      .max(30, "Too Long")
-      .required("Required"),
-    number: Yup.string()
-      .min(3, "Too Short")
-      .max(30, "Too Long")
-      .required("Required"),
+    name: Yup.string().min(3, "Too Short").max(30, "Too Long").required("Required"),
+    number: Yup.string().min(3, "Too Short").max(30, "Too Long").required("Required"),
   });
 
-  const initialValues = {
-    name: "",
-    number: "",
-  };
-
-  const nameFieldId = useId();
-  const numberFieldID = useId();
+  const initialValues = { name: "", number: "" };
 
   const handleSubmit = (values, actions) => {
-    dispatch(addContact({ ...values, id: Date.now() }));
+    dispatch(addContact(values));
     actions.resetForm();
   };
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={UserSchema}
-      onSubmit={handleSubmit}
-    >
+    <Formik initialValues={initialValues} validationSchema={UserSchema} onSubmit={handleSubmit}>
       <Form className={css.container}>
         <div className={css.inputWrap}>
           <label className={css.labelText}>Name</label>
-          <Field className={css.fieldInput} type="text" name="name" id={nameFieldId} />
+          <Field className={css.fieldInput} type="text" name="name" />
           <ErrorMessage className={css.errormsg} name="name" component="div" />
         </div>
         <div className={css.inputWrap}>
           <label className={css.labelText}>Number</label>
-          <Field className={css.fieldInput} type="text" name="number" id={numberFieldID} />
+          <Field className={css.fieldInput} type="text" name="number" />
           <ErrorMessage className={css.errormsg} name="number" component="div" />
         </div>
         <button className={css.button} type="submit">Add Contact</button>
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,28 +1,17 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { selectContacts, deleteContact } from '../../redux/contactsSlice';
-import { selectNameFilter } from '../../redux/filtersSlice';
+import { useSelector } from 'react-redux';
+import { selectFilteredContacts } from '../../redux/contactsSlice';
 import Contact from '../Contact/Contact';
 import css from './ContactList.module.css';
 
 export default function ContactList() {
-  const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectNameFilter);
-
-  const visibleContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
-  const handleDeleteContact = (contactId) => {
-    dispatch(deleteContact(contactId));
-  };
+  const visibleContacts = useSelector(selectFilteredContacts);
 
   return (
     <div className={css.container}>
       <ul className={css.card}>
         {visibleContacts.map((contact) => (
           <li className={css.cardWrap} key={contact.id}>
-            <Contact id={contact.id} name={contact.name} number={contact.number} onDelete={() => handleDeleteContact(contact.id)} />
+            <Contact id={contact.id} name={contact.name} number={contact.number} />
           </li>
         ))}
       </ul>
